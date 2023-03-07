29cb0477a1c88b6c6abf166e3dc586dfdaae1875
AmyWilford <wilford.amy@gmail.com>
2023-03-07 15:21:20 -0500

    added require path in server.js

--- server/server.js
+++ server/server.js
@@ -1,20 +1,19 @@
 const express = require("express");
+const path = require("path");
 const db = require("./config/connection");
 const routes = require("./routes");
 
-// const cwd = process.cwd();
 
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
-// app.get("/*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
-// });
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 db.once("open", () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
