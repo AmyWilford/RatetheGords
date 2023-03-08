d0e5785dfb40f8e41bf8b970385a3ccd0380a90c
AmyWilford <wilford.amy@gmail.com>
2023-03-08 14:56:35 -0500

    attempt to seed heroku from utils.seed.js

--- server/utils/seed.js
+++ server/utils/seed.js
@@ -1,15 +1,13 @@
-const db = require('../config/connection');
-const { Gord } = require('../models');
+const db = require("../config/connection");
+const { Gord } = require("../models");
 
-const gordData = require('./gordData.json');
+const gordData = require("./gordData.json");
 
-db.once('open', async () => {
+db.once("open", async () => {
   await Gord.deleteMany({});
-
-
   await Gord.collection.insertMany(gordData);
 
-  console.log('all done!');
+  console.log("all done!");
   console.table(gordData);
   process.exit(0);
 });
